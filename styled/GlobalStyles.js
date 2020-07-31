9954f33133ca3f1f0b58ad797338f394d5dc8278
Weider Lima <weider.lima@mercadolivre.com>
2020-07-31 16:03:59 -0300

    Improve styles

--- styled/GlobalStyles.js
+++ styled/GlobalStyles.js
@@ -8,15 +8,19 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    background: #e1e1e1;
-    color: #333;
+    background: #212e38;
+    color: #00b3a4;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     margin: 0 0 48px 0;
     padding: 0;
-    font-family: sans-serif;
+    font-family: -apple-system,BlinkMacSystemFont, Segoe UI, sans-serif;
     font-size: 14px;
     transition: all 0.25s linear;
+  }
+  
+  h1, h2, h3, h4, p {
+    text-shadow: 0 2px 4px rgba(0,0,0,.2);
   }`;
